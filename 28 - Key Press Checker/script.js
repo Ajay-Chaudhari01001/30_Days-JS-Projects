e83039409b3d1552a3fe50ecdcfad93ce990a248
Ajay Chaudhari <55138445+Ajay-Chaudhari01001@users.noreply.github.com>
2022-09-16 06:56:20 +0530

    Add files via upload

---
+++ 28 - Key Press Checker/script.js
@@ -0,0 +1,56 @@
+ 
+
+
+
+ window.addEventListener("keydown", (e) => {
+    document.getElementById(
+      "result"
+    ).innerHTML = `The key pressed is <span>${e.key}</span><span>Key Code: ${e.keyCode}</span>`;
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Simple key press and detect the key press enter name and that ascii code value
